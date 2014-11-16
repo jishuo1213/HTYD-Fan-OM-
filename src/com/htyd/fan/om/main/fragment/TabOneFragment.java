b9641234a6cadb62878b982db314fa776639378f
Fan <jishuo1213@126.com>
2014-11-16 22:32:31 +0800

    主页
    主页GridView布局

--- src/com/htyd/fan/om/main/fragment/TabOneFragment.java
+++ src/com/htyd/fan/om/main/fragment/TabOneFragment.java
@@ -1,47 +1,116 @@
 package com.htyd.fan.om.main.fragment;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.htyd.fan.om.R;
-import com.htyd.fan.om.attendmanage.AttentManagerActivity;
-import com.htyd.fan.om.map.OMLocationManager;
 
 public class TabOneFragment extends Fragment {
-
-	private OMLocationManager mLocationManager;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mLocationManager = OMLocationManager.get(getActivity());
 	}
-
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.test_layout, container, false);
-		Button getLoc = (Button) v.findViewById(R.id.btn_get_location);
-		getLoc.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mLocationManager.startLocationUpdates();
-				Intent i = new Intent(getActivity(), AttentManagerActivity.class);
-				startActivity(i);
-			}
-		});
+		View v = inflater
+				.inflate(R.layout.fragment_main_page, container, false);
+		intiView(v);
 		return v;
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+	}
+
+	private void intiView(View v) {
+		GridView mGridView = (GridView) v.findViewById(R.id.grid_main_page);
+		mGridView.setAdapter(new MainPageGridAdapter());
+	}
+
+	private class MainPageGridAdapter extends BaseAdapter {
+		private String[] itemArray;
+		private int[] colorIdArray;
+		private Resources r;
+
+		public MainPageGridAdapter() {
+			r = getActivity().getResources();
+			itemArray = r.getStringArray(R.array.main_page_text);
+			TypedArray colors = r
+					.obtainTypedArray(R.array.main_page_item_color);
+			colorIdArray = new int[colors.length()];
+			for (int i = 0; i < colors.length(); i++) {
+				colorIdArray[i] = r.getColor(colors.getResourceId(i, -1));
+				Log.i("fanjishuo____colorIdArray", "colorIdArray" + i
+						+ colorIdArray[i] + "");
+			}
+			colors.recycle();
+		}
+
+		@Override
+		public int getCount() {
+			return itemArray.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return itemArray[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@SuppressLint("InflateParams")
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder mHolder;
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(
+						R.layout.main_page_item_layout, null);
+				mHolder = new ViewHolder(convertView);
+				convertView.setTag(mHolder);
+			} else {
+				mHolder = (ViewHolder) convertView.getTag();
+			}
+
+			mHolder.setText((String) getItem(position));
+
+			if (colorIdArray[position] != -1) {
+				mHolder.setBackground(colorIdArray[position]);
+			}
+			return convertView;
+		}
+	}
+
+	private class ViewHolder {
+
+		private TextView itemTextView;
+
+		public ViewHolder(View v) {
+			itemTextView = (TextView) v.findViewById(R.id.tv_item_menu);
+		}
+
+		public void setText(String text) {
+			itemTextView.setText(text);
+		}
+
+		public void setBackground(int color) {
+			itemTextView.setBackgroundColor(color);
+		}
 	}
 }
