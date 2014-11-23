8ec0775ec32f9c9f64c8fb606d6278cf378e0d0a
Fan <jishuo1213@126.com>
2014-11-23 23:01:34 +0800

    tasklist界面

--- src/com/htyd/fan/om/model/TaskDetailBean.java
+++ src/com/htyd/fan/om/model/TaskDetailBean.java
@@ -1,26 +1,30 @@
 package com.htyd.fan.om.model;
 
+import java.text.SimpleDateFormat;
+
+import android.annotation.SuppressLint;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class TaskDetailBean implements Parcelable {
 
-	public String workProvince;//任务所在省份
-	public String workCity;//任务所在市
-	public String workDistrict;//任务所在县区
-	public String installLocation;//安装地点
-	public String taskDescription;//任务描述
-	public String taskContacts;//任务联系人
-	public String contactsPhone;//联系人手机
-	public String recipientsName;//任务领取人
-	public String recipientPhone;//领取人电话
-	public String taskAccessory;//任务附件
-	public String equipment;//设备
-	public String productType;//产品类别
-	public long planStartTime;//计划开始时间
-	public long planEndTime;//计划结束时间
-	public int taskState;//任务状态//0:在处理任务 1:待领取任务 2:已完成任务
-	public int taskType;//任务类别
+	public String workProvince;// 任务所在省份
+	public String workCity;// 任务所在市
+	public String workDistrict;// 任务所在县区
+	public String installLocation;// 安装地点
+	public String taskTitle;//任务标题
+	public String taskDescription;// 任务描述
+	public String taskContacts;// 任务联系人
+	public String contactsPhone;// 联系人手机
+	public String recipientsName;// 任务领取人
+	public String recipientPhone;// 领取人电话
+	public String taskAccessory;// 任务附件
+	public String equipment;// 设备
+	public String productType;// 产品类别
+	public long planStartTime;// 计划开始时间
+	public long planEndTime;// 计划结束时间
+	public int taskState;// 任务状态//0:在处理任务 1:待领取任务 2:已完成任务
+	public int taskType;// 任务类别
 
 	@Override
 	public int describeContents() {
@@ -33,6 +37,7 @@ public class TaskDetailBean implements Parcelable {
 		dest.writeString(workCity);
 		dest.writeString(workDistrict);
 		dest.writeString(installLocation);
+		dest.writeString(taskTitle);
 		dest.writeString(taskDescription);
 		dest.writeString(taskContacts);
 		dest.writeString(contactsPhone);
@@ -48,13 +53,13 @@ public class TaskDetailBean implements Parcelable {
 	}
 
 	public static Parcelable.Creator<TaskDetailBean> CREATOR = new Creator<TaskDetailBean>() {
-		
+
 		@Override
 		public TaskDetailBean[] newArray(int size) {
-			
+
 			return new TaskDetailBean[size];
 		}
-		
+
 		@Override
 		public TaskDetailBean createFromParcel(Parcel source) {
 			TaskDetailBean mBean = new TaskDetailBean();
@@ -62,6 +67,7 @@ public class TaskDetailBean implements Parcelable {
 			mBean.workCity = source.readString();
 			mBean.workDistrict = source.readString();
 			mBean.installLocation = source.readString();
+			mBean.taskTitle = source.readString();
 			mBean.taskDescription = source.readString();
 			mBean.taskContacts = source.readString();
 			mBean.contactsPhone = source.readString();
@@ -77,10 +83,16 @@ public class TaskDetailBean implements Parcelable {
 			return mBean;
 		}
 	};
-	
+
 	public String getDetailAddress() {
 		StringBuilder sb = new StringBuilder();
 		return sb.append(workProvince).append(workCity).append(workDistrict)
 				.append(installLocation).toString();
+	}
+
+	@SuppressLint("SimpleDateFormat")
+	public String getTime() {
+		return new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss")
+				.format(this.planStartTime);
 	}
 }
--- src/com/htyd/fan/om/taskmanage/fragment/TaskListFragment.java
+++ src/com/htyd/fan/om/taskmanage/fragment/TaskListFragment.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Loader;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.htyd.fan.om.R;
 import com.htyd.fan.om.model.TaskDetailBean;
@@ -22,7 +24,6 @@ import com.htyd.fan.om.util.db.OMUserDatabaseHelper.TaskCursor;
 import com.htyd.fan.om.util.db.OMUserDatabaseManager;
 import com.htyd.fan.om.util.loaders.SQLiteCursorLoader;
 import com.htyd.fan.om.util.ui.CustomChooserView.OnItemChooserListener;
-import com.htyd.fan.om.util.ui.UItoolKit;
 
 public class TaskListFragment extends Fragment implements OnItemChooserListener {
 
@@ -30,91 +31,145 @@ public class TaskListFragment extends Fragment implements OnItemChooserListener 
 	private static final String INPROCESSINGTASK = "inprocessingtask";
 	private static final String BERECEIVE = "bereceive";
 	private static final String COMPLETED = "completed";
-	private HashMap<String,List<TaskDetailBean>> taskMap;
+	private HashMap<String, List<TaskDetailBean>> taskMap;
 	private ListView mListView;
-	
-	
+	private TaskAdapter allTaskAdapter, inProcessTaskAdapter, beReceiveAdapter,
+			completedAdapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		taskMap = new HashMap<String, List<TaskDetailBean>>(); 
+		taskMap = new HashMap<String, List<TaskDetailBean>>();
+		allTaskAdapter = new TaskAdapter(-1);
+		inProcessTaskAdapter = new TaskAdapter(0);
+		beReceiveAdapter = new TaskAdapter(1);
+		completedAdapter = new TaskAdapter(2);
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_tasklist_layout, container, false);
+		View v = inflater.inflate(R.layout.fragment_tasklist_layout, container,
+				false);
 		initView(v);
 		return v;
 	}
 
 	private void initView(View v) {
+		mListView = (ListView) v.findViewById(R.id.list_my_task);
 	}
 
 	@Override
 	public void onItemChooser(int position) {
-		UItoolKit.showToastShort(getActivity(), "position"+position);
+		switch (position) {
+		case 0:
+			mListView.setAdapter(inProcessTaskAdapter);
+			break;
+		case 1:
+			mListView.setAdapter(beReceiveAdapter);
+			break;
+		case 2:
+			mListView.setAdapter(completedAdapter);
+			break;
+		}
 	}
-	
-	private class TaskAdapter extends BaseAdapter{
-		
-		private int type;//0 在处理 1 待领取 2已完成
-		
-		
-		
+
+	private class TaskAdapter extends BaseAdapter {
+
+		private int type;// 0 在处理 1 待领取 2已完成
+		private int inProcessTaskNum, beReceiveNum, completedNum;
+
 		public TaskAdapter(int type) {
 			super();
 			this.type = type;
+			inProcessTaskNum = taskMap.get(INPROCESSINGTASK).size();
+			beReceiveNum = taskMap.get(BERECEIVE).size();
+			completedNum = taskMap.get(COMPLETED).size();
 		}
 
 		@Override
 		public int getCount() {
-			switch(type){
+			switch (type) {
 			case 0:
-				return taskMap.get(INPROCESSINGTASK).size();
+				return inProcessTaskNum;
 			case 1:
-				return taskMap.get(BERECEIVE).size();
+				return beReceiveNum;
 			case 2:
-				return taskMap.get(COMPLETED).size();
+				return completedNum;
+			case -1:
+				return inProcessTaskNum + beReceiveNum + completedNum;
 			}
 			return 0;
 		}
 
 		@Override
 		public Object getItem(int position) {
-			switch(type){
+			switch (type) {
 			case 0:
-				return taskMap.get(INPROCESSINGTASK).size();
+				return taskMap.get(INPROCESSINGTASK).get(position);
 			case 1:
-				return taskMap.get(BERECEIVE).size();
+				return taskMap.get(BERECEIVE).get(position);
 			case 2:
-				return taskMap.get(COMPLETED).size();
+				return taskMap.get(COMPLETED).get(position);
+			case -1:
+				if (position < inProcessTaskNum) {
+					return taskMap.get(INPROCESSINGTASK).get(position);
+				} else if (position >= inProcessTaskNum
+						&& position < (inProcessTaskNum + beReceiveNum - 1)) {
+					return taskMap.get(BERECEIVE).get(
+							position - inProcessTaskNum);
+				} else {
+					return taskMap.get(COMPLETED).get(
+							position - inProcessTaskNum - beReceiveNum);
+				}
 			}
 			return null;
 		}
 
 		@Override
 		public long getItemId(int position) {
-			
-			return 0;
+			return position;
 		}
 
+		@SuppressLint("InflateParams")
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			
-			return null;
+			ViewHolder mHolder;
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(
+						R.layout.task_item_layout, null);
+				mHolder = new ViewHolder(convertView);
+				convertView.setTag(mHolder);
+			} else {
+				mHolder = (ViewHolder) convertView.getTag();
+			}
+			TaskDetailBean mBean = (TaskDetailBean) getItem(position);
+			mHolder.taskDescrption.setText(mBean.taskDescription);
+			mHolder.taskCreateTime.setText(mBean.getTime());
+			mHolder.taskState.setText(mBean.taskState + "");
+			return convertView;
 		}
-		
 	}
-	
-	
-	public static class TaskCursorLoader extends SQLiteCursorLoader{
+
+	private class ViewHolder {
+		public TextView taskDescrption, taskCreateTime, taskState;
+
+		// public ImageView taskStateIcon;
+		public ViewHolder(View v) {
+			taskDescrption = (TextView) v.findViewById(R.id.tv_task_descrption);
+			taskCreateTime = (TextView) v.findViewById(R.id.tv_task_createtime);
+			taskState = (TextView) v.findViewById(R.id.tv_task_state);
+			// taskStateIcon = (ImageView)
+			// v.findViewById(R.id.img_task_state_icon);
+		}
+	}
+
+	public static class TaskCursorLoader extends SQLiteCursorLoader {
 
 		private int taskState;
 		private OMUserDatabaseManager mManager;
-		
-		public TaskCursorLoader(Context context,int state) {
+
+		public TaskCursorLoader(Context context, int state) {
 			super(context);
 			this.taskState = state;
 			mManager = OMUserDatabaseManager.getInstance(context);
@@ -134,10 +189,9 @@ public class TaskListFragment extends Fragment implements OnItemChooserListener 
 			return loadCursor();
 		}
 	}
-	
-	private class TaskCursorCallback  implements LoaderCallbacks<Cursor>{
 
-		
+	private class TaskCursorCallback implements LoaderCallbacks<Cursor> {
+
 		@Override
 		public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 			return new TaskCursorLoader(getActivity(), args.getInt(TASKSTATE));
@@ -146,28 +200,30 @@ public class TaskListFragment extends Fragment implements OnItemChooserListener 
 		@Override
 		public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 			List<TaskDetailBean> mList = new ArrayList<TaskDetailBean>();
-			if(data != null && data.moveToFirst()){
+			if (data != null && data.moveToFirst()) {
 				TaskCursor taskCursor = (TaskCursor) data;
 				mList.add(taskCursor.getTask());
 			}
-			for(TaskDetailBean mBean:mList){
-				switch(mBean.taskState){
-				case 0://在处理任务
+			for (TaskDetailBean mBean : mList) {
+				switch (mBean.taskState) {
+				case 0:// 在处理任务
 					taskMap.get(INPROCESSINGTASK).add(mBean);
 					break;
-				case 1://待领取任务
+				case 1:// 待领取任务
 					taskMap.get(BERECEIVE).add(mBean);
 					break;
-				case 2://已完成任务
+				case 2:// 已完成任务
 					taskMap.get(COMPLETED).add(mBean);
 					break;
 				}
+			}
+			if (mListView != null) {
+				mListView.setAdapter(allTaskAdapter);
 			}
 		}
 
 		@Override
 		public void onLoaderReset(Loader<Cursor> loader) {
 		}
-		
 	}
 }
