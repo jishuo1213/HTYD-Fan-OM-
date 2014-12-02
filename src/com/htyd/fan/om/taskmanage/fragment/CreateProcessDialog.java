9326ef0ae206c52dd320561a019ae3deab2ec1da
Fan <jishuo1213@126.com>
2014-12-02 21:26:37 +0800

    一个布局

---
+++ src/com/htyd/fan/om/taskmanage/fragment/CreateProcessDialog.java
@@ -0,0 +1,21 @@
+package com.htyd.fan.om.taskmanage.fragment;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class CreateProcessDialog extends DialogFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		
+		return super.onCreateDialog(savedInstanceState);
+	}
+
+}
